e4f4dbc22ec5c9f549cc7d7d7510ed78c6891756
Mecharis <121699952+mecharis-420@users.noreply.github.com>
2023-10-25 20:57:27 -0400

    Update types.ts

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Apps{
+    name: string
+    pkg: number
+    description: string
+    version?: number
+    author?: string
+};
+
+export interface wm{
+    size?: number
+    width?: number
+    name: string
+}
