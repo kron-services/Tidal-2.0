c267a33bd416a0cdca23dcaa6e3f8473a6a08b44
Mecharis <121699952+mecharis-420@users.noreply.github.com>
2023-10-28 20:51:01 -0400

    pls work for once god dam

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,13 @@ export interface App {
   open: (data?: any) => Promise<void>;
   isCustom?: boolean; // Add this property
 }
+
+export interface WM {
+  title: string
+  icon: string
+  width?: number
+  height?: number
+  canResize?: boolean
+  minWidth?: number
+  minHeight?: number
+}
